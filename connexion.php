fd362daf1c6e6edb7fac6f908bf1476d43e5f388
Alexis <alexis.bonis1712@gmail.com>
2020-04-13 16:47:31 +0200

    modification du style de la page de connexion

---
+++ connexion.php
@@ -0,0 +1,77 @@
+
+<!doctype html>
+<html lang="fr">
+<link href="css/styles.css" rel="stylesheet" />
+
+<head>
+  <meta charset="utf-8">
+  <title>Page Connexion</title>
+</head>
+
+<body>
+  <!-- à compléter -->
+</br>
+</br>
+  <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Connexion</h2>
+  <div class="divider-custom">
+    <div class="divider-custom-line"></div>
+    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+    <div class="divider-custom-line"></div>
+  </div>
+  <form action="connexion.php" method="POST">
+    <div class="container">
+      <div class="fillform" style="margin: 1rem;">
+        <div class="control-group">
+          <div class="form-group floating-label-form-group controls mb-0 pb-2">
+            <label for="pseudo"><b>Pseudo:</b></label><input class="form-control" type="text" name="pseudo" placeholder="Pseudo *" required="required" data-validation-required-message="Entrez votre pseudo s'il vous plaît." />
+            <p class="help-block text-danger"></p>
+          </div>
+        </div>
+        <div class="control-group">
+          <div class="form-group floating-label-form-group controls mb-0 pb-2">
+            <label for="mdp"><b>Mot de passe:</b></label></label><input class="form-control" type="password" name="mdp" placeholder="Mot de passe *" required="required" data-validation-required-message="Entrez votre mot de passe s'il vous plaît." />
+            <p class="help-block text-danger"></p>
+          </div>
+        </div>
+      </div>
+      <div class="butt" style="text-align: center; margin: 1rem;">
+        <button class="btn btn-primary btn-xl" type="reset">Annuler</button>
+        <button class="btn btn-primary btn-xl" type="submit" name="valider">Se connecter</button></div>
+      </div>
+    </div>
+    <div style="text-align: center; margin: 1rem;"> <a href="./inscription.php">Première connexion ?</a> </div>
+
+  </form>
+</body>
+
+</html>
+
+<?php
+session_start();
+require_once 'fonctions/bd.php';
+require_once 'fonctions/utilisateur.php';
+
+$stateMsg = "";
+
+if (isset($_POST["valider"])) {
+  $pseudo = $_POST["pseudo"];
+  $hashMdp = md5($_POST["mdp"]);
+
+  $link = getConnection($dbHost, $dbUser, $dbPwd, $dbName);
+  $check = getUser($pseudo, $hashMdp, $link);
+
+  if (getUser($pseudo, $hashMdp, $link) == TRUE) {
+    $_SESSION["pseudo"]= $pseudo;
+    $_SESSION["mdp"]= $hashMdp;
+    setConnected($pseudo, $link);
+    header('Location: index.html?subscribe=yes');
+    exit();
+  } 
+  else {
+    $stateMsg = "Le couple pseudo/mot de passe ne correspond à aucun utilisateur enregistré";
+  }
+echo $stateMsg;
+}
+
+
+?>
--- inscription.php
+++ inscription.php
@@ -1,3 +1,59 @@
+
+<!doctype html>
+<html lang="fr">
+<link href="css/styles.css" rel="stylesheet" />
+
+<head>
+  <meta charset="utf-8">
+  <title>Inscription</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+  <!-- à compléter -->
+</br>
+</br>
+  <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Inscription</h2>
+  <div class="divider-custom">
+    <div class="divider-custom-line"></div>
+    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+    <div class="divider-custom-line"></div>
+  </div>
+  <form action="inscription.php" method="POST">
+    <div class="container">
+      <div class="fillform" style="margin: 1rem;">
+        <div class="control-group">
+          <div class="form-group floating-label-form-group controls mb-0 pb-2">
+            <label for="pseudo"><b>Pseudo souhaité:</b></label><input class="form-control" type="text" name="pseudo" placeholder="Pseudo souhaité *" required="required" data-validation-required-message="Entrez votre pseudo s'il vous plaît." />
+            <p class="help-block text-danger"></p>
+          </div>
+        </div>
+        <div class="control-group">
+          <div class="form-group floating-label-form-group controls mb-0 pb-2">
+            <label for="mdp"><b>Mot de passe:</b></label></label><input class="form-control" type="password" name="mdp" placeholder="Mot de passe *" required="required" data-validation-required-message="Entrez votre mot de passe s'il vous plaît." />
+            <p class="help-block text-danger"></p>
+          </div>
+        </div>
+        <div class="control-group">
+          <div class="form-group floating-label-form-group controls mb-0 pb-2">
+            <label for="mdp"><b>Répétez le mot de passe: </b></label></label><input class="form-control" type="password" name="mdp-repeat" placeholder="Répétez le mot de passe *" required="required" data-validation-required-message="Entrez votre mot de passe s'il vous plaît." />
+            <p class="help-block text-danger"></p>
+          </div>
+        </div>
+      </div>
+      <div class="butt" style="text-align: center; margin: 1rem;">
+        <button class="btn btn-primary btn-xl" type="reset">Annuler</button>
+        <button class="btn btn-primary btn-xl" type="submit" name="valider">S'inscrire</button>
+      </div>
+    </div>
+    <div style="text-align: center; margin: 1rem;"> <a href="./connexion.php">Déjà inscrit ?</a> </div>
+  </form>
+
+</body>
+
+</html>
+
 <?php
 session_start();
 require_once 'fonctions/bd.php';
@@ -16,7 +72,7 @@ if (isset($_POST["valider"])) {
   if ($hashMdp == $hashConfirmMdp) {
     if ($available) {
       register($pseudo, $hashMdp, $link);
-      header('Location: index.php?subscribe=yes');
+      header('Location: connexion.php?subscribe=yes');
       exit();
     } else {
       echo "Le pseudo demand&eacute; est d&eacute;j&agrave; utilis&eacute;";
@@ -27,52 +83,3 @@ if (isset($_POST["valider"])) {
 }
 // echo isset($_POST["valider"]);
 ?>
-
-<!doctype html>
-<html lang="fr">
-
-<head>
-  <meta charset="utf-8">
-  <title>Inscription</title>
-  <link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-  <style>
-    body {
-      width: 50%;
-      padding: 10% 22%; 
-      justify-content: center;
-      /* text-align: center; */
-    }
-  </style>
-
-  <!-- à compléter -->
-  <h1 style="text-align: center;">Inscription à la BDW</h1>
-  <form action="inscription.php" style="border:2px solid #ccc; border-radius: 30px;" method="POST">
-    <div class="container">
-      <div class="fillform" style="margin: 1rem;">
-        <label for="pseudo"><b>Pseudo souhaité:</b></label>
-        <input type="text" placeholder="Entrer un pseudo" name="pseudo" required>
-        <br>
-        <br>
-        <label for="mdp"><b>Mot de passe:</b></label>
-        <input type="password" placeholder="Entrer MDP" name="mdp" required>
-        <br>
-        <br>
-        <label for="psw-repeat"><b>Confirmer mot de passe:</b></label>
-        <input type="password" placeholder="Confirmer mdp" name="mdp-repeat" required>
-        <br>
-        <br>
-      </div>
-      <div class="butt" style="text-align: center; margin: 1rem;">
-        <button type="button" class="cancelbtn">Annuler</button>
-        <button type="submit" class="valider" name="valider">S'inscrice</button>
-      </div>
-    </div>
-    <div style="text-align: center; margin: 1rem;"> <a href="./index.php">Déjà inscrit ?</a> </div>
-  </form>
-
-</body>
-
-</html>
