2ba989236cc6c9266a8f8092d6874bede1ef91e9
Alexis <alexis.bonis1712@gmail.com>
2020-04-25 15:26:01 +0200

    ajout modal pour chaque image

--- fonctions/requetesql.php
+++ fonctions/requetesql.php
@@ -26,18 +26,40 @@
             $image=$_POST['categorie'];
             if($image=='Tout')
             {
-                $query = "SELECT nomFich FROM Photo";
+                $query = "SELECT C.nomCat, P.nomFich,`description` FROM Photo P JOIN Categorie C ON C.catId=P.catId; ";
             }
             else
             {
-                $query = "SELECT photoId,nomFich FROM `Photo` WHERE catId =$Idcategorie; ";
+                $query = "SELECT C.nomCat, P.photoId, P.nomFich,`description` FROM Photo P JOIN Categorie C ON C.catId=P.catId WHERE catId =$Idcategorie; ";
             }
         }
         $resultat = executeQuery($link, $query);
         $i = 0;
         foreach($resultat as $r)
         {
-            echo '<img src="assets/img/'.$r['nomFich'].'"/> ';
+            echo '<img src="assets/img/'.$r['nomFich'].'" data-toggle="modal" data-target="#'.$r['nomFich'].'" type="button" class="card card-tall"/>
+            </img>
+            <div class="modal fade" id="'.$r['nomFich'].'">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content" style="width:650px;">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel"> '.$r['nomFich'].'</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <img src="assets/img/'.$r['nomFich'].'"  style="width: 250px;height: 250px;margin-right:20px;" align="left">
+
+                            <div>Nom de la photo: '.$r['nomFich'].'</div>
+                            </br>
+                            <div>Sa description: '.$r['description'].'</div>
+                            </br>
+                            <div>Et sa catégorie: '.$r['nomCat'].'</div>
+                        </div>
+                    </div>
+                </div>
+            </div>';
             $i = $i+1;
         }
     }
--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
+        <!-- Js Bootstrap -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+        </script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+        </script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="Une nouvelle variante de Pinterest produite par des étudiants !" />
@@ -51,7 +59,7 @@
                 </div>
                 <label>Selectionnez la catégorie :</label>
                 <form action="index.php#galerie" method="POST">
-                    <SELECT id="categorie" name="categorie" required>
+                    <SELECT id="categorie" name="categorie">
                         <OPTION name="Tout"> Tout</OPTION>
                         <?php
                             require_once("fonctions/requetesql.php");
